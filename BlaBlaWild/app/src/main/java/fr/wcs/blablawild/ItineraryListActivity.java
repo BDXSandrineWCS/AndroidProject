a9b3bdfe2a79825ff98b635b36ec62db7b833561
sandrine <poupardsandrine@gmail.com>
2018-11-18 22:40:39 +0100

    steps: coder l'activité qui affiche les resultats :finished

--- BlaBlaWild/app/src/main/java/fr/wcs/blablawild/ItineraryListActivity.java
+++ BlaBlaWild/app/src/main/java/fr/wcs/blablawild/ItineraryListActivity.java
@@ -1,5 +1,6 @@
 package fr.wcs.blablawild;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,12 @@ public class ItineraryListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_itinerary_list);
+
+        // Get the Intent that started this activity (from ItinerarySearchActivity) and extract the extra (departure destination and date)
+        Intent intent = getIntent();
+        String departure = intent.getStringExtra(ItinerarySearchActivity.EXTRA_DEPARTURE);
+        String destination = intent.getStringExtra(ItinerarySearchActivity.EXTRA_DESTINATION);
+        String date = intent.getStringExtra(ItinerarySearchActivity.EXTRA_DATE);
+        if ((departure != null) && (destination!= null)) this.setTitle(departure + " >> " + destination);
     }
 }
